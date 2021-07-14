f69147c70f62eb8234a6a528dae97c416903fe0b
Raymund De La Victoria <raymunddlv@gmail.com>
2021-07-14 17:31:47 +0800

    modified

--- routes/students.js
+++ routes/students.js
@@ -14,7 +14,7 @@ const Student = require('../models/student')
 
 router.get('/', async(request, response) => {
     try{
-            const students = await Student.find(request.contacts)
+            const students = await Student.find()
             response.json(students)
     }catch(err){
         response.send('Error' + err)
