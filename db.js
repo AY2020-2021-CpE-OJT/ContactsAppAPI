c21163f74219efcbebcb64b196eaf75890042b13
Raymund De La Victoria <raymunddlv@gmail.com>
2021-07-17 17:48:33 +0800

    modified

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/StudentDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/myFirstDatabase', { useNewUrlParser: true, useUnifiedTopology: true })
         .then(connect => console.log('connected to mongodb..'))
         .catch(e => console.log('could not connect to mongodb', e))
 
